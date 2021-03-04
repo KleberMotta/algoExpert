3b752f691fa625d6be36f4e023f5b931339e84c1
Kleber Motta <kleber.outlook@gmail.com>
2021-03-04 11:46:54 -0300

    nonConstructibleChange algoExpert solution

--- Algorithms/nonConstructibleChange_2.js
+++ Algorithms/nonConstructibleChange_2.js
@@ -1,24 +1,23 @@
+// algoExpet solution
+
 // n(n log(n)) t | O(1) s
 function nonConstructibleChange(coins) {
   // Write your code here.
-  let possibleChange = 0;
   coins.sort((a, b) => a - b);
 
-  for (i = 0; i < coins.length; i++) {
-
-    if (coins[i] > possibleChange + 1) {
-      return possibleChange + 1;
-    }
-    possibleChange += coins[i];
+  let curentChangeCreated = 0;
+  for (const coin of coins) {
+    if (coin > curentChangeCreated + 1) return curentChangeCreated + 1;
+    curentChangeCreated += coin;
   }
 
-  return possibleChange + 1;
+  return curentChangeCreated + 1;
 }
 
 // Do not edit the line below.
 exports.nonConstructibleChange = nonConstructibleChange;
 
-const result = nonConstructibleChange([6, 4, 5, 1, 1, 8, 9]);
+const result = nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]);
 console.log(result);
 
 // Do not edit the line below.
