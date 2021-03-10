5c0dca104b27e6edf85a58c54da1ad9b52752776
Kleber Motta <kleber.outlook@gmail.com>
2021-03-10 14:41:41 -0300

    findClosestValueInBst my solution

--- Algorithms/easy/findClosestValueInBst_1.js
+++ Algorithms/easy/findClosestValueInBst_1.js
@@ -1,11 +1,20 @@
 function findClosestValueInBst(tree, target) {
 
-  if (tree.value > target && tree.left != null)
-    return findClosestValueInBst(tree.left, target);
-  if (tree.value < target && tree.right != null)
-    return findClosestValueInBst(tree.right, target);
+  let closest = tree.value;
 
-  return tree.value;
+  while (tree != null) {
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+      closest = tree.value;
+    }
+    if (tree.value > target) {
+      tree = tree.left;
+    } else if (tree.value < target) {
+      tree = tree.right;
+    } else {
+      break;
+    }
+  }
+  return closest;
 }
 
 // This is the class of the input tree. Do not edit.
