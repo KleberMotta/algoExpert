eace039f65593b06d60956fc5426aff5e7d955b7
Kleber Motta <kleber.outlook@gmail.com>
2021-03-10 16:43:53 -0300

    branchSums algoExpert solution

---
+++ Algorithms/easy/branchSums_2.js
@@ -0,0 +1,34 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// O(n) time | O(n) Space - where n is the number of nodes in the Binary Tree
+function branchSums(root) {
+  const sums = [];
+  calculateBranchSums(root, 0, sums);
+  return sums;
+}
+
+function calculateBranchSums(node, runningSum, sums) {
+
+  if (!node) return;
+
+  const newRunningSum = runningSum + node.value;
+  if (!node.left && !node.right) {
+    sums.push(newRunningSum);
+    return;
+  }
+
+  calculateBranchSums(node.left, newRunningSum, sums);
+  calculateBranchSums(node.right, newRunningSum, sums);
+}
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.branchSums = branchSums;
