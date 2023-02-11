fb9b5660c3343b71f6640ebe0b99878ce5a0180f
Kleber Motta <kleber.motta@olxbr.com>
2023-02-11 12:16:55 -0300

    Add semordnilap

--- Algorithms/easy/semordnilap.ts
+++ Algorithms/easy/semordnilap.ts
@@ -1,16 +1,31 @@
-export function semordnilap(words: string[]) {
-  let result: string[][]
-  let map = new Map<string, string>()
+function verifyPalindrome(a: string, b: string) {
+  if (a.length !== b.length) return false
+  const reversedB = Array.from(b).reverse().join('')
+  return a === reversedB
+}
 
-  for (let i=0; i < words.length; i++) {
-    const currentStringA = words[i]
-    for (let j=0; i < words.length; j++) {
-      const currentStringB = words[j]
-      if (map.get(currentStringA+currentStringB) || 
-          currentStringA === currentStringB) {
-        continue;
+export function semordnilap(words: string[]) {
+  const result: string[][] = []
+  const registerMap = new Map<string, boolean>()
+
+  for (let a = 0; a < words.length; a++) {
+    const currentStringA = words[a]
+    for (let b = 0; b < words.length; b++) {
+      const currentStringB = words[b]
+      if (
+        a === b ||
+        registerMap.get(currentStringA + currentStringB) ||
+        registerMap.get(currentStringB + currentStringA)
+      ) {
+        continue
       }
+
+      if (verifyPalindrome(currentStringA, currentStringB)) {
+        result.push([currentStringA, currentStringB])
+      }
+
+      registerMap.set(currentStringA + currentStringB, true)
     }
   }
-  return []
+  return result
 }
